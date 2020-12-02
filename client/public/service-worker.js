97d7a0150076b648e05d3f26390ef9478d216ec3
Illuminaxx <nexgen62@hotmail.fr>
2020-12-02 18:17:15 +0100

    update sw.js

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -9,7 +9,7 @@ var urlsToCache = [
 
 
 // Install SW
-this.addEventListener('install', event => {
+this.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache);
@@ -26,18 +26,18 @@ this.addEventListener('fetch', event => {
                 return response;
             }
 
-            return fetch(event.request);
+            //return fetch(event.request);
         })
     );
 });
 
 //Update SW
-this.addEventListener('activate', event => {
+this.addEventListener('activate', (event) => {
     var cacheWhitelist = ['wms-pwa'];
     event.waitUntil(
         caches.keys().then(function(cacheNames)  {
             return Promise.all(
-                cacheNames.map(function(cacheName) {
+                cacheNames.map((cacheName) => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
                     }
