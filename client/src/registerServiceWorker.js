b0b587caf95d49f2e57e728ed0be91171d432f01
Illuminaxx <nexgen62@hotmail.fr>
2020-12-02 15:45:17 +0100

    update sw

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/registerServiceWorker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       //const swUrl = `./registerServiceWorker.js`;
 
       if (isLocalhost) {
