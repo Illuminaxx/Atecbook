f7d1ad40171fb27dd6460b34f5d2b7f3ca0f8dbe
Illuminaxx <nexgen62@hotmail.fr>
2020-12-08 14:45:50 +0100

    remove compression

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const router = require("./routes/api/router");
 const path = require("path");
-const compression = require('compression');
+
 
 const app = express();
 const PORT = process.env.PORT || 3003;
@@ -15,7 +15,6 @@ app.use("/api", router);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression());
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
