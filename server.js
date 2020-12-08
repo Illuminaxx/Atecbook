08a6612726da12aaaaac59846773da9a66453d18
Illuminaxx <nexgen62@hotmail.fr>
2020-12-08 14:38:31 +0100

    update server

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(express.json({ useUrlExtended: false }));
 connectDB();
 app.use("/api", router);
 
-app.use(compression());
+
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
