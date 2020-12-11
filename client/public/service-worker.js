2675a6f21b97f24df4de6846fccd68719f5601dc
Illuminaxx <nexgen62@hotmail.fr>
2020-12-11 17:01:44 +0100

    update service worker

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,7 +1,7 @@
+importScripts('https://unpkg.com/dexie@latest/dist/dexie.js')
+
 var doCache = true;
-
 var CACHE_NAME = "atec-book-cache";
-
 var urlsToCache = [
     '/static/css/main.5d019410.chunk.css',
     '/static/js/2.86c0399e.chunk.js',
@@ -57,6 +57,150 @@ self.addEventListener('fetch', event => {
                 
             })
         );
+
+        if(event.request.method === "POST") {
+            var database = new Dexie("req_cache");
+            database.version(1).stores({
+                req_cache: 'key,response,timestamp'
+            })
+
+            event.respondWith(
+                fetch(event.request.clone()).then(function(response) {
+                    cachePut(event.request.clone(), response.clone(), database.req_cache)
+                    return response
+                })
+                .catch(function() {
+                    return cacheMatch(event.request.clone(), database.req_cache)
+                })
+            )
+
+        }
+
     }
 });
 
+/**
+ * Serializes a Request into a plain JS object
+ *
+ *
+ * @param request
+ * @returns Promise
+ */
+function serializeRequest(request) {
+    var serialized = {
+        url: request.url,
+        headers: serializeHeaders(request.headers),
+        method: request.method,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        referrer: request.referrer
+    }
+
+    if(request.method !== 'GET' && request.method !== 'HEAD') {
+        return request.clone().text().then(function(body) {
+            serialized.body = body
+            return Promise.resolve(serialized)
+        })
+    }
+    return Promise.resolve(serialized)
+}
+
+/**
+* Serializes a Response into a plain JS object
+*
+* @param response
+* @returns Promise
+*/
+function serializeResponse(response) {
+    var serialized = {
+        headers: serializeHeaders(response.headers),
+        status: response.status,
+        statusText: response.statusText
+    }
+
+    return response.clone().text().then(function(body) {
+        serialized.body = body
+        return Promise.resolve(serialized)
+    })
+}
+
+/**
+* Serializes headers into a plain JS object
+* 
+* @param headers
+* @returns object
+*/
+function serializeHeaders(headers) {
+    var serialized = {}
+    for(var entry of headers.entries()) {
+        serialized[entry[0]] = entry[1]
+    }
+    return serialized
+} 
+
+/**
+* Creates a Response from it's serialized version
+* 
+* @param data
+* @returns Promise
+*/
+function deserializeResponse(data) {
+   return Promise.resolve(new Response(data.body, data))
+}
+
+/**
+* Saves the response for the given request eventually overriding the previous version
+* 
+* @param data
+* @returns Promise
+*/
+function cachePut(request, response, store) {
+    var key, data;
+    getPostId(request.clone()).then(function(id) {
+        key = id;
+        return serializeResponse(response.clone())
+    }).then(function(serializedResponse) {
+        data = serializedResponse;
+        var entry = {
+            key: key,
+            response: data,
+            timestamp: Date.now()
+        }
+        store
+            .add(entry)
+            .catch(function(error) {
+                store.update(entry.key, entry)
+            })
+    })
+}
+
+/**
+* Returns the cached response for the given request or an empty 503-response  for a cache miss.
+* 
+* @param request
+* @return Promise
+*/
+function cacheMatch(request, store) {
+    return getPostId(request.clone())
+    .then(function(id) {
+        return store.get(id)
+    }).then(function(data) {
+        if (data) {
+			return deserializeResponse(data.response);
+		} else {
+			return new Response('', {status: 503, statusText: 'Service Unavailable'});
+		}
+    })
+}
+
+/**
+* Returns a string identifier for our POST request.
+* 
+* @param request
+* @return string
+*/
+function getPostId(request) {
+	return JSON.stringify(serializeRequest(request.clone()));
+}
