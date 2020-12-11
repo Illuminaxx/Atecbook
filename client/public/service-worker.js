d03134277b8c61613a513cadad4cfd860afbf197
Illuminaxx <nexgen62@hotmail.fr>
2020-12-11 10:41:58 +0100

    http request 1112

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -52,7 +52,7 @@ self.addEventListener('fetch', event => {
     if(doCache) {
         event.respondWith(
             caches.match(event.request).then(function(response) {
-                console.log('Request event: ' + event.request)
+                console.log('Request event: ' + event.request.url)
                 return response || fetch(event.request)
                 
             })
