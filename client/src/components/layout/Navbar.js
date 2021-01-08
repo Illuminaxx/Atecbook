504b1596c36a8540742ac69b8ed20668e8868588
Illuminaxx <nexgen62@hotmail.fr>
2021-01-08 18:54:23 +0100

    update logo

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       <h1>
         <Link to="/">
           {/*<i className="fa fa-commenting" aria-hidden="true"></i> ATEC'book */}
-          <img src={logo} alt="Logo Atecna" />
+          <img className="logo" src={logo} alt="Logo Atecna" />
         </Link>
       </h1>
       <>{isAuthenticated ? authLinks : guestLinks}</>
