a73f078c7ac17305b97e6d0e5f62c1234de70660
aurelien <ahennuyer@datainmotion.fr>
2023-03-23 07:39:58 +0100

    🎨 fix error in store

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,8 @@
-import {createStore, applyMiddleware} from 'redux';
+import {applyMiddleware, legacy_createStore as createStore} from 'redux';
+
 import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const initialState = {};
 const middleWare = [thunk];
